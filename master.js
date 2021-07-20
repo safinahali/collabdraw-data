b2803ee06bb65316a1c88ef4689c68b753bf218e
Safinah Ali <safinahaali@gmail.com>
2021-07-20 17:03:01 -0400

    clean data

--- master.js
+++ master.js
@@ -2,140 +2,138 @@ let data;
 let extension;
 var y=100;
 var j = 100;
-var threshold = 50;
 // change this to log from different rooms
 var logroom = "2";
-var ms = 0;
-var threshold = 500;
-var timer;
-var speed = 200;
-var startTime = 16000000000000;
+var loground = "0";
 var chatKeysPrinted = [];
-var roomSelect = document.getElementById('roomselect');
+var sortedEpochs = [];
+var drawEpochs = [];
 
 function preload(){
-	filename = 'room-2-valid.json'
+	filename = 'collaborative-draw-clean-default-rtdb-export.json'
 	data = loadJSON(filename);	
  }
 
 function setup() {
 	createCanvas(windowWidth, 3000);
 	background(250);
-
-	text("Data logged from room: " + logroom, 20, 20);
-	timer = millis();
+	sortedEpochs = sortEpochs();
+	console.log(sortedEpochs);
+	makeDrawings(logroom);
 	// noLoop();
-	text("Speed", 600,10);
-	slider = createSlider(1, 25, 10, 5);
-    slider.position(600, 20);
-	slider.style('width', '80px');
-	//for getting first draw : need to fix this
-
-// 	for (var element in data){
-// 		 var draws = data[element]
-// 		 draws = Object.values(data[element])
-// 	}
-// 	 // sort draws by epoch
-// 	 // draws.sort((a,b) => {a.epoch > b.epoch}? 1: -1)
-// 	 console.log("first time is " + draws[0]["epoch"]);
 }
 
 function changeroom(){
 	clear();
 	background(250);
-	logroom = roomSelect.value;
+	var roomSelect = document.getElementById('roomselect');
+	var logroom = roomSelect.value;
+	makeDrawings(logroom);
 	return;
 }
 
+// function changeround(){
+// 	clear();
+// 	background(250);
+// 	var roundSelect = document.getElementById('roundselect');
+// 	var loground = roundSelect.value;
+// 	return;
+// }
 
-function draw() {
-	ms = millis();
-	// speed = slider.value();
-	
+function sortEpochs(){
 	for (var element in data){
 		var elementname = JSON.stringify(element);
+		//if it is a drawing from room 0
+		if (elementname.includes("room"+logroom+":") && elementname.includes("drawings")){
+			for (var key in data[element]) {
+				if("epoch" in data[element][key]){
+					drawEpochs.push(data[element][key].epoch);
+					sortedEpochs = drawEpochs.sort();
+				}
+			}
+		}
+	}
+	return sortedEpochs;
+}
 
+function makeDrawings(logroom) {
+	for (var element in data){
+		var elementname = JSON.stringify(element);
+		//if it is a drawing from room 0
 		if (elementname.includes("room"+logroom+":") && elementname.includes("drawings")){
 
 			for (var key in data[element]) {
-				var threshold = 500;
-
-				if("epoch" in data[element][key]){
-					if(data[element][key].epoch < startTime){
-						startTime = data[element][key].epoch;
-					}
-				}
-
+				
+				//actual drawing info data table
 				if ("table" in data[element][key]) {
 					
-						for (i = 0; i<data[element][key].table.length; i++){
-							noFill();
-							strokeWeight(8);
+					for (i = 0; i<data[element][key].table.length; i++){
+						noFill();
+						strokeWeight(12);
 
-							var round = data[element][key].table[i].round;
-					      	
-				      		if(data[element][key].table[i].pinfo.playername == 0){
-				      			stroke(80);
-				      		}
+						loground = data[element][key].table[i].round;
 
-				      		else{
-				      			stroke(120);
-				      		}
-				      		
-				      		//this is the first epoch in data
-				      		if(ms>(data[element][key].epoch-startTime)/speed){
-				      			line(data[element][key].table[i].x, data[element][key].table[i].y+500*round, data[element][key].table[i].px, data[element][key].table[i].py+500*round);
+						//change this to a condition for loground
+						// if(data[element][key].table[i].round == loground){
+							//go by the epochs and add 500 for new drawings
+							// for(j = 0; j<sortedEpochs.length; j++){
+								// if(sortedEpochs[3] < data[element][key].epoch < sortedEpochs[4]){
+									//different colors for different users
+									console.log(data[element][key].table[i].pinfo.playername);
+						      		if(data[element][key].table[i].pinfo.playername%2 == 0){
+						      			stroke(100,0,20);
+						      		}
+						      		else{
+						      			stroke(20,100,0);
 
-				      			var currentpath = document.getElementById('thispart');
-				      			currentpath.innerHTML = "STATUS: round: " + round + " player: " + data[element][key].table[i].pinfo.playername + " part: " +  data[element][key].table[i].part ;
-							}
+						      		}
 
-						threshold = threshold + 20;
+						      		line(data[element][key].table[i].x+50, data[element][key].table[i].y+500*loground, data[element][key].table[i].px+50, data[element][key].table[i].py+500*loground);
+								// }
+							// }
+						// }
 					} 
 				}
 			}
 		}
 
 
-		else if (elementname.includes("room"+logroom+":") && elementname.includes("chat")){
-			y=100;
+		// else if (elementname.includes("room"+logroom+":") && elementname.includes("chat")){
+		// 	y=100;
 			
 
-			for (var key in data[element]) {
-				ms = millis();
-				var round = parseInt(data[element][key].round);	
-
+		// 	for (var key in data[element]) {
+		// 		var round = parseInt(data[element][key].round);	
 				
-				if (ms>(data[element][key].epoch-startTime)/speed){
-					// var currentchat = document.getElementById('thischat');
-					// currentchat.innerHTML = "CHAT MSG: " + data[element][key].chat;
+		// 		if (ms>(data[element][key].epoch-startTime)/speed){
+		// 			// var currentchat = document.getElementById('thischat');
+		// 			// currentchat.innerHTML = "CHAT MSG: " + data[element][key].chat;
 
-					noStroke();
-					fill(0);
-
+		// 			noStroke();
+		// 			fill(0);
 					
-					if (chatKeysPrinted.includes(key)==false){
-						chatKeysPrinted.push(key);
-						// this should work when pno is logged correctly with chat
-						textFont('Georgia', 18);
-						text("player: " + data[element][key].pno + " in round: " + data[element][key].round, 600, y+300*round);
+		// 			if (chatKeysPrinted.includes(key)==false){
+		// 				chatKeysPrinted.push(key);
+		// 				// this should work when pno is logged correctly with chat
+		// 				textFont('Georgia', 18);
+		// 				text("player: " + data[element][key].pno + " in round: " + data[element][key].round, 600, y+300*round);
 						
-						text(data[element][key].chat, 850, y+300*round);
-						y = y+35;	
-					}	
-				}
-			}
+		// 				text(data[element][key].chat, 850, y+300*round);
+		// 				y = y+35;	
+		// 			}	
+		// 		}
+		// 	}
 
-		}
+		// }
 
 		else if (elementname.includes("room"+logroom+":") && elementname.includes("initial")){			
 			for (var key in data[element]) {
 				stroke(50,102,152);
-			    strokeWeight(8);
+			    strokeWeight(12);
 			    noFill();
-			    bezier(data[element][key][0], data[element][key][1], data[element][key][2], data[element][key][3], data[element][key][4], data[element][key][5], data[element][key][6], data[element][key][7]);
-			    bezier(data[element][key][8], data[element][key][9]+500, data[element][key][10], data[element][key][11]+500, data[element][key][12], data[element][key][13]+500, data[element][key][14], data[element][key][15]+500);
-			    bezier(data[element][key][16], data[element][key][17]+1000, data[element][key][18], data[element][key][19]+1000, data[element][key][20], data[element][key][21]+1000, data[element][key][22], data[element][key][23]+1000);
+			    bezier(data[element][key][0]+50, data[element][key][1], data[element][key][2]+50, data[element][key][3], data[element][key][4]+50, data[element][key][5], data[element][key][6]+50, data[element][key][7]);
+			    bezier(data[element][key][8]+50, data[element][key][9]+500, data[element][key][10]+50, data[element][key][11]+500, data[element][key][12]+50, data[element][key][13]+500, data[element][key][14]+50, data[element][key][15]+500);
+			    bezier(data[element][key][16]+50, data[element][key][17]+1000, data[element][key][18]+50, data[element][key][19]+1000, data[element][key][20]+50, data[element][key][21]+1000, data[element][key][22]+50, data[element][key][23]+1000);
 			}
 
 		}
